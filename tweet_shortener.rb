ea85e243d7ee6c3d6e5611855a6cf711d72293a3
Amir Safoev <amirsafoev0@gmail.com>
2018-10-22 12:31:49 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+require 'pry'
+def dictionary
+  dictionary_hash = { "hello": 'hi', "to, two, too": '2', "for, four": '4', 'be': 'b', 'you': 'u', "at": '@', "and": '&'}
+end
+def word_substituter(tweet_one)
+puts tweet_one.to_a
+binding.pry 
+end
