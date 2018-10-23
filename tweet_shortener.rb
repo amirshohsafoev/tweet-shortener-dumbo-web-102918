b6a18d4ebb7f7047a5605f69d4d8ca6f7b57f793
Amir Safoev <amirsafoev0@gmail.com>
2018-10-23 19:08:34 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,28 +4,30 @@ def dictionary
   dictionary_hash = { "hello" => 'hi', "to" => '2', "two" => '2', "too" => '2', "for" => '4', "four" => '4', 'be' => 'b', 'you'=> 'u', "at" => '@', "and" => '&'}
 end
 def word_substituter(tweet_one)
-
-array = tweet_one.split()
-#binding.pry
-#dictionary.keys.select {|a| a == array}
-array.collect do |string| 
-
-  #binding.pry
-  #string.include?(dictionary.keys)
-  dictionary.map do |k, v|
- if  string.include?(k) 
-  string.gsub!(v)
-end    
-end.flatten.compact
-end
+array = tweet_one.split(" ")
+ array.collect do |string|
+  if dictionary.keys.include?(string)
+      dictionary[string]
+    else
+       string
+      #binding.pry
+  end
+ # array.join(" ")
+end.join(" ")
 end
 
-"def word_substituter(tweet_one)
+"def word_substituter(tweet_one, tweet_one_short)
 array = tweet_one.split()
-dictionary.map do |keys, value|
+array.collect do |string|
+dictionary.map do |key, value|
   #keys.each  do |k|
   #binding.pry
-array.include?(keys)
-#binding.pry
+if string.include?(key)
+  tweet_one_short
+else
+  tweet_one
+  #binding.pry
+end
+end
 end
 end"
